46d343253ce2ca2f7696b7deae5be670b1ea14cc
keronidas <djulian1992@gmail.com>
2024-10-26 18:03:41 +0200

    das

---
+++ src/assets/scripts/audio.js
@@ -0,0 +1,4 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const audio = document.getElementById('background-audio');
+    audio.muted = false;  // Desmutea el audio después de que la página haya cargado
+});
